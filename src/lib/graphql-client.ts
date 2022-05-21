c123a2bb92ec8e789cfa1489fac05e096e254021
Aivo Olev <aivo.olev@gmail.com>
2022-05-21 23:47:49 +0300

    Env fix

--- src/lib/graphql-client.ts
+++ src/lib/graphql-client.ts
@@ -1,4 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
-export const GRAPHQL_ENDPOINT = 'http://localhost:4444'; // process.env['GRAPHQL_ENDPOINT']
+import { API_ENDPOINT } from '../../env';
+export const GRAPHQL_ENDPOINT = API_ENDPOINT;
 
 export const client = new GraphQLClient(GRAPHQL_ENDPOINT, { credentials: 'include' });
