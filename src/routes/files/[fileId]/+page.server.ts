c3f0f44015182fb5ad0456a8f77fba5fa6115ef2
Aivo Olev <aivo.olev@gmail.com>
2022-11-11 11:09:11 +0000

    Handle peaks generation errors better

--- src/routes/files/[fileId]/+page.server.ts
+++ src/routes/files/[fileId]/+page.server.ts
@@ -21,25 +21,44 @@ export const load: PageServerLoad = async ({ params, locals, url }) => {
             await fs.access(file.path).catch(e => fileExists = false);
             console.log('file', fileExists)
             if (fileExists) {
+                let failed = false;
                 const peaksDone = new Promise((resolve, reject) => {
                     const generatePeaks = spawn('audiowaveform', ['-i', file.path, '-o', peaksPath, '--pixels-per-second', '20', '--bits', '8']);
                     generatePeaks.on('exit', function (code) {
                         console.log('generate peaks exited with code ' + code);
-                        if (code === 2) reject(new Error("File generation error"))
+                        if (code === 1 || code == 2) {
+                            failed = true;
+                        }
                         resolve(true);
                     })
                 })
+                await peaksDone.catch(e => {
+                    peaksExist = false;
+                    console.log(e);
+                })
+                if (failed) {
+                    return {
+                        file: {
+                            id: file.id,
+                            state: file.state,
+                            content: content,
+                            path: file.path,
+                            name: file.filename,
+                            uploadedAt: file.uploadedAt
+                        },
+                        url: url.origin,
+                        peaks: null
+                    }
+                }
                 peaksExist = true;
-                await peaksDone.catch(e => peaksExist = false)
                 const normalizeDone = new Promise((resolve, reject) => {
                     const normalize = spawn('python', ['./scripts/normalize_peaks.py', peaksPath]);
                     normalize.on('exit', function (code) {
                         console.log('normalize exited with code ' + code);
-                        if (code === 2) reject(new Error("File generation error"))
                         resolve(true);
                     })
                 })
-                await normalizeDone.catch(e => peaksExist = false)
+                await normalizeDone.catch(e => console.log(e))
             }
         }
         let peaks = null;
