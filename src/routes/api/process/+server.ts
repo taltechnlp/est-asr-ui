73665f4643c01a0e2a530bcdf9744fe585cfa8bc
Aivo Olev <aivo.olev@gmail.com>
2025-02-06 17:45:23 +0200

    Improve error handling

--- src/routes/api/process/+server.ts
+++ src/routes/api/process/+server.ts
@@ -9,7 +9,8 @@ import { sendMail, createEmail } from "$lib/email";
 export const POST: RequestHandler = async ({ request, fetch }) => {
     const workflow: IWeblog = await request.json();
     if (!workflow) {
-        return error(400, { message: "Nextflow sent an empty POST message." })
+        console.error("Nextflow sent an empty POST message.");
+        return new Response("Unexpected POST message format", { status: 400 });
     } else {
         const file = await prisma.file.findFirst({
             where: {
@@ -28,95 +29,104 @@ export const POST: RequestHandler = async ({ request, fetch }) => {
         });
         // trace is only provided for the following events: process_submitted, process_started, process_completed, error
         if (workflow.trace) {
-            await prisma.nfWorkflow.upsert({
-                where: {
-                    run_name: file.externalId
-                },
-                create: {
-                    run_id: workflow.runId,
-                    event: workflow.event,
-                    run_name: workflow.runName,
-                    file: {
-                        connect: {
-                            id: file.id
+            try {
+                await prisma.nfWorkflow.upsert({
+                    where: {
+                        run_name: file.externalId
+                    },
+                    create: {
+                        run_id: workflow.runId,
+                        event: workflow.event,
+                        run_name: workflow.runName,
+                        file: {
+                            connect: {
+                                id: file.id
+                            }
+                        },
+                        utc_time: new Date(workflow.utcTime),
+                        processes: {
+                            create: {
+                                task_id: workflow.trace.task_id,
+                                process: workflow.trace.process,
+                                tag: workflow.trace.tag,
+                                hash: workflow.trace.hash,
+                                status: workflow.trace.status,
+                                exit: workflow.trace.exit ? new Date(workflow.trace.exit) : null,
+                                submit: workflow.trace.submit ? new Date(workflow.trace.submit) : null,
+                                start: workflow.trace.start ? new Date(workflow.trace.start) : null,
+                                complete: workflow.trace.complete ? new Date(workflow.trace.complete) : null,
+                                duration: workflow.trace.duration,
+                                realtime: workflow.trace.realtime,
+                            }
                         }
                     },
-                    utc_time: new Date(workflow.utcTime),
-                    processes: {
-                        create: {
-                            task_id: workflow.trace.task_id,
-                            process: workflow.trace.process,
-                            tag: workflow.trace.tag,
-                            hash: workflow.trace.hash,
-                            status: workflow.trace.status,
-                            exit: workflow.trace.exit ? new Date(workflow.trace.exit) : null,
-                            submit: workflow.trace.submit ? new Date(workflow.trace.submit) : null,
-                            start: workflow.trace.start ? new Date(workflow.trace.start) : null,
-                            complete: workflow.trace.complete ? new Date(workflow.trace.complete) : null,
-                            duration: workflow.trace.duration,
-                            realtime: workflow.trace.realtime,
+                    update: {
+                        run_id: workflow.runId,
+                        // event: workflow.event,  Avoid overwriting the workflow status / event
+                        run_name: workflow.runName,
+                        file: {
+                            connect: {
+                                id: file.id
+                            }
+                        },
+                        utc_time: new Date(workflow.utcTime),
+                        processes: {
+                            create: {
+                                task_id: workflow.trace.task_id,
+                                process: workflow.trace.process,
+                                tag: workflow.trace.tag,
+                                hash: workflow.trace.hash,
+                                status: workflow.trace.status,
+                                exit: workflow.trace.exit ? new Date(workflow.trace.exit) : null,
+                                submit: workflow.trace.submit ? new Date(workflow.trace.submit) : null,
+                                start: workflow.trace.start ? new Date(workflow.trace.start) : null,
+                                complete: workflow.trace.complete ? new Date(workflow.trace.complete) : null,
+                                duration: workflow.trace.duration,
+                                realtime: workflow.trace.realtime,
+                            }
                         }
                     }
-                },
-                update: {
-                    run_id: workflow.runId,
-                    // event: workflow.event,  Avoid overwriting the workflow status / event
-                    run_name: workflow.runName,
-                    file: {
-                        connect: {
-                            id: file.id
-                        }
-                    },
-                    utc_time: new Date(workflow.utcTime),
-                    processes: {
-                        create: {
-                            task_id: workflow.trace.task_id,
-                            process: workflow.trace.process,
-                            tag: workflow.trace.tag,
-                            hash: workflow.trace.hash,
-                            status: workflow.trace.status,
-                            exit: workflow.trace.exit ? new Date(workflow.trace.exit) : null,
-                            submit: workflow.trace.submit ? new Date(workflow.trace.submit) : null,
-                            start: workflow.trace.start ? new Date(workflow.trace.start) : null,
-                            complete: workflow.trace.complete ? new Date(workflow.trace.complete) : null,
-                            duration: workflow.trace.duration,
-                            realtime: workflow.trace.realtime,
-                        }
-                    }
-                }
-            }).catch(e => console.log("save NF info to db failed", e))
+                    
+                })
+            }
+            catch(e) {console.log("save NF info to db failed", e)}
             
         } 
         else if (workflow.event === "error") {
-            await prisma.nfWorkflow.upsert({
-                where: {
-                    run_name: file.externalId
-                },
-                create: {
-                    run_id: workflow.runId,
-                    event: workflow.event,
-                    run_name: workflow.runName,
-                    file: {
-                        connect: {
-                            id: file.id
-                        }
+            try {
+                await prisma.nfWorkflow.upsert({
+                    where: {
+                        run_name: file.externalId
                     },
-                    utc_time: new Date(workflow.utcTime),
-                    failedCount: 1,
-                },
-                update: {
-                    run_id: workflow.runId,
-                    event: workflow.event,
-                    run_name: workflow.runName,
-                    file: {
-                        connect: {
-                            id: file.id
-                        }
+                    create: {
+                        run_id: workflow.runId,
+                        event: workflow.event,
+                        run_name: workflow.runName,
+                        file: {
+                            connect: {
+                                id: file.id
+                            }
+                        },
+                        utc_time: new Date(workflow.utcTime),
+                        failedCount: 1,
                     },
-                    utc_time: new Date(workflow.utcTime),
-                    failedCount: 1,
-                }
-            }).catch(e => console.log("save NF info to db failed", e))
+                    update: {
+                        run_id: workflow.runId,
+                        event: workflow.event,
+                        run_name: workflow.runName,
+                        file: {
+                            connect: {
+                                id: file.id
+                            }
+                        },
+                        utc_time: new Date(workflow.utcTime),
+                        failedCount: 1,
+                    }
+                })
+            }
+            catch(e) {
+                console.log("save NF info to db failed", e)
+            }
         }
         // metadata is only provided for the following events: started, completed
         else if (workflow.metadata) {
