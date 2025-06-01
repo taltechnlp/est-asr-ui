a8a8e494f530e0d6e9261d1fb0942edee9a28a5c
Aivo Olev <aivo.olev@gmail.com>
2025-06-01 10:30:26 +0300

    removed unnecessary files

--- src/routes/[[lang]]/annotation/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-	import Editor from '$lib/components/editor/editor.svelte'
-</script>
-
-<Editor data="<p>See on redigeeritav tekst</p>" fileId="1"></Editor>
